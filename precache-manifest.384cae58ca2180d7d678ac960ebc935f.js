585cd7c6fbc71c443c6c62672a7899db63b55b89
Pavel-Denchev <pdenchev@developsoft.com>
2023-04-04 12:41:40 +0300

    Updates

---
+++ precache-manifest.384cae58ca2180d7d678ac960ebc935f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0215a6fdb6f37b4307f4126a89d2aaef",
+    "url": "/xor-demo/static/media/Motiva.Art-Medium.0215a6fd.otf"
+  },
+  {
+    "revision": "f28762f1315e03569f553eaa3c6e691e",
+    "url": "/xor-demo/static/media/Motiva-Regular.f28762f1.otf"
+  },
+  {
+    "revision": "d73c7b3f5f8329810af4",
+    "url": "/xor-demo/static/js/runtime~main.427d0f63.js"
+  },
+  {
+    "revision": "66267730858f83d196b5",
+    "url": "/xor-demo/static/js/main.b440d817.chunk.js"
+  },
+  {
+    "revision": "1dfd17ea1ae2264110f1",
+    "url": "/xor-demo/static/js/3.7b78a1f0.chunk.js"
+  },
+  {
+    "revision": "a2f539057274999feef0",
+    "url": "/xor-demo/static/js/2.13f1e84a.chunk.js"
+  },
+  {
+    "revision": "66267730858f83d196b5",
+    "url": "/xor-demo/static/css/main.9a828550.chunk.css"
+  },
+  {
+    "revision": "359101642aa036b8670045bf527a29eb",
+    "url": "/xor-demo/index.html"
+  }
+];
